49a4ed7c8fc401247a4b090bf2fcb938d922dd08
leebang <leebang31698@gmail.com>
2023-04-26 15:49:09 -0400

    FilmSubscription migration added

---
+++ Migrations/20230426183931_AddFilmSubscription.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFilmSubscription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FilmSubscription",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    FilmId = table.Column<int>(type: "integer", nullable: false),
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FilmSubscription", x => new { x.UserId, x.FilmId });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FilmSubscription");
+        }
+    }
+}
