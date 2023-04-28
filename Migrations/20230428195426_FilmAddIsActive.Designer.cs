3aacf57f05e75fadf4489eaed4bc3314b3a9459a
leebang <leebang31698@gmail.com>
2023-04-28 17:15:23 -0400

    Migration changes

--- Migrations/20230428195426_FilmAddIsActive.Designer.cs
+++ Migrations/20230428195426_FilmAddIsActive.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace dotnet.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230428195426_FilmAddIsActive")]
+    partial class FilmAddIsActive
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -76,8 +79,7 @@ namespace dotnet.Migrations
 
                     b.HasIndex("FilmId");
 
-                    b.HasIndex("UserId", "FilmId")
-                        .IsUnique();
+                    b.HasIndex("UserId");
 
                     b.ToTable("FilmSubscription");
                 });
