be0f59ad15524f99556c8eee692dd244d21b2d06
leebang <leebang31698@gmail.com>
2023-04-28 17:12:56 -0400

    FilmSubscriptionEntity Remove Key

--- Crates/FilmSubscription/FilmSubscriptionEntity.cs
+++ Crates/FilmSubscription/FilmSubscriptionEntity.cs
@@ -5,8 +5,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 public class FilmSubscription
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public int UserId { get; set; }
     public int FilmId { get; set; }
