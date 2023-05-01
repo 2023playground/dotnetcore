edbeecb7f0828d5815591f00e695ac9e94a51f96
nekonekokun <32277391+derekli19951103@users.noreply.github.com>
2023-05-01 14:04:15 -0400

    use 0.0.0.0 specifically

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.Listen(
-        IPAddress.Loopback,
+        IPAddress.Parse("0.0.0.0"),
         int.Parse(port!),
         listenOptions =>
         {
@@ -28,7 +28,7 @@ builder.WebHost.ConfigureKestrel(options =>
     );
 
     options.Listen(
-        IPAddress.Loopback,
+        IPAddress.Parse("0.0.0.0"),
         int.Parse(port!) + 1,
         listenOptions =>
         {
