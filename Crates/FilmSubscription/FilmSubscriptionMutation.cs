9a96b902ee3aad4e2723073b6dddc4daf49ac9b5
leebang <leebang31698@gmail.com>
2023-05-02 10:31:11 -0400

    FilmSubMutation: remove checking

--- Crates/FilmSubscription/FilmSubscriptionMutation.cs
+++ Crates/FilmSubscription/FilmSubscriptionMutation.cs
@@ -8,46 +8,26 @@ public partial class Mutation
     public FilmSubscription CreateFilmSubscription(AppDbContext db, int userId, int filmId)
     {
         var filmSubscription = new FilmSubscription { UserId = userId, FilmId = filmId };
-        // Check if user and film exists
-        if (
-            db.Users.Any(u => u.Id == userId)
-            && db.Films.Any(
-            f => f.Id == filmId
-            && f.HasSessions == false
-            && f.IsActivate == true
-            ))
+
+        // Add FilmSubscription if user did not subscribe to this film
+        try
         {
-            // Add FilmSubscription if user did not subscribe to this film
-            if (db.FilmSubscription.Any(fs => fs.UserId == userId && fs.FilmId == filmId))
-            {
-                throw new QueryException(ErrorBuilder.New().SetMessage("User already subscribed to this film").Build());
-            }
-            else
-            {
-                try
-                {
-                    db.FilmSubscription.Add(filmSubscription);
-                    db.SaveChanges();
-                    return filmSubscription;
-                }
-                catch (Exception)
-                {
-                    throw new QueryException(ErrorBuilder.New().SetMessage("Error during creating Film Subscription.").Build());
-                }
-            }
+            db.FilmSubscription.Add(filmSubscription);
+            db.SaveChanges();
+            return filmSubscription;
         }
-        else
+        catch (Exception)
         {
-            // TODO: Provide more information about error
-            throw new QueryException(ErrorBuilder.New().SetMessage("User or Film not available").Build());
+            throw new QueryException(ErrorBuilder.New().SetMessage("Error during creating Film Subscription.").Build());
         }
+
 
     }
 
     // Delete Film Subscription
-    public bool DeleteFilmSubscription(AppDbContext db, int userId, int filmId)
+    public bool DeleteFilmSubscription(AppDbContext db, int subscriptionId)
     {
-        var filmSubscription = db.FilmSubscription.FirstOrDefault(f => f.UserId == userId && f.FilmId == filmId);
+        var filmSubscription = db.FilmSubscription.FirstOrDefault(f => f.Id == subscriptionId);
         if (filmSubscription == null)
         {
             throw new QueryException(ErrorBuilder.New().SetMessage("User or Film not found").Build());
